8a85955c677485dd9f0ff9a817051e68ddd53b6e
Mikhail Kalinin <mikhail.kalinin.05@list.ru>
2023-12-04 00:12:31 +0300

    2 geometry.h

--- 5.Geometry/geometry.h
+++ 5.Geometry/geometry.h
@@ -2,6 +2,7 @@
 #include <vector>
 #include <initializer_list>
 #include <cmath>
+#define PI 3.14
 
 struct Point {
   double x;
@@ -10,6 +11,8 @@ struct Point {
   Point(double x, double y) : x(x), y(y) {}
 
   friend bool operator==(const Point&, const Point&);
+
+  friend double points_distance(const Point&, const Point&);
 };
 
 bool operator==(const Point& point1, const Point& point2) {
@@ -20,13 +23,19 @@ bool operator!=(const Point& point1, const Point& point2) {
   return !(point1 == point2);
 }
 
+double points_distance(const Point& point1, const Point& point2) {
+  return pow((point1.x - point2.x) * (point1.x - point2.x) + (point1.y - point2.y) * (point1.y - point2.y), 0.5);
+}
+
 class Line {
 // Ax + By + C = 0
   double a = 0.0;
   double b = 0.0;
   double c = 0.0;
-
+public:
   Line() = default;
+
+  Line(double a, double b, double c) : a(a), b(b), c(c) {}
 
   Line(const Point& point1, const Point& point2) {
     a = point1.y - point2.y;
@@ -52,19 +61,26 @@ bool operator!=(const Line& line1, const Line& line2) {
 }
 
 class Vector {
-  Point a = Point(0.0, 0.0);
-  Point b = Point(0.0, 0.0);
+  double x = 0.0;
+  double y = 0.0;
 public:
-  Vector(const Point& a, const Point& b) : a(a), b(b) {}
+  Vector(double x, double y) : x(x), y(y) {}
 
-  double len() const { return pow((a.x + b.x) * (a.x + b.x) + (a.y + b.y) * (a.y + b.y), 0.5); }
+  Vector(const Point& a, const Point& b) : x(a.x - b.x), y(a.y - b.y) {}
+
+  friend double scalarProduct(const Vector&, const Vector&);
 };
+
+double scalarProduct(const Vector& vec1, const Vector& vec2) {
+  return vec1.x * vec2.x + vec1.y * vec2.y;
+}
 
 class Shape {
 
 };
 
 class Polygon : Shape {
+protected:
   std::vector<Point> vertices_;
   int vertices_count_;
 public:
@@ -78,6 +94,57 @@ public:
   std::vector<Point> getVertices() { return vertices_; };
 
   bool isConvex() {
+    for (int i = 0; i < vertices_count_; ++i) {
+      Vector vec1 = Vector(vertices_[i], vertices_[(i + 1) % vertices_count_]);
+      Vector vec2 = Vector(vertices_[(i + 1) % vertices_count_], vertices_[(i + 2) % vertices_count_]);
+      if (scalarProduct(vec1, vec2) < 0) {
+        return false;
+      }
+    }
+    return true;
+  }
+};
 
+class Ellipse : Shape {
+  // x^2/a^2 + y^2/b^2 = 1;
+protected:
+  double a = 0.0;
+  double b = 0.0;
+  std::pair<Point, Point> focus = std::pair(Point(0, 0), Point(0, 0));
+public:
+  Ellipse(const Point& point1, const Point& point2, double dist) {
+    focus = std::make_pair(point1, point2);
+    a = dist / 2;
+    b = pow((a * a - (points_distance(point1, point2) / 2) * (points_distance(point1, point2) / 2)), 0.5);
+  }
+
+  std::pair<Point, Point> focuses() const { return focus; }
+
+  std::pair<Line, Line> directrices() const {
+    Line line1 = Line(1, 0, a / eccentricity());
+    Line line2 = Line(1, 0, (-1 * a) / eccentricity());
+    return std::make_pair(line1, line2);
+  }
+
+  double eccentricity() const { return pow((1 - (a * a / b * b)), 0.5); }
+
+  Point center() const {
+    return Point((focus.first.x + focus.second.x) / 2, (focus.first.y + focus.second.y) / 2);
+  }
+};
+
+class Circle : public Ellipse {
+  Circle(const Point& center, double radius) : Ellipse(center, center, 2 * radius) {}
+
+  double radius() const {return a; }
+};
+
+class Rectangle : public Polygon {
+  Rectangle(const Point& point1, const Point& point2, double k) : Polygon{point1, Point(0, 0), point2, Point(0, 0)} {
+
+    double angle = 180 - 2 * atan(k) * 180 / PI;
+    Point middle = middle_point(point1, point2);
+    vertices[1] = point1.rotate(middle, angle);
+    vertices[3] = point2.rotate(middle, angle);
   }
 };
