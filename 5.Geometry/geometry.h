cc8794b003878103fe9c090887e839d57e18b0c9
Mikhail Kalinin <mikhail.kalinin.05@list.ru>
2023-12-13 21:20:26 +0300

    More geometry!!!

--- 5.Geometry/geometry.h
+++ 5.Geometry/geometry.h
@@ -3,7 +3,7 @@
 #include <initializer_list>
 #include <cmath>
 
-#define PI 3.141592
+#define NUM_PI 3.14159265
 
 class Line;
 
@@ -16,18 +16,18 @@ struct Point {
   Point(double x, double y) : x(x), y(y) {}
 
   Point rotate(const Point& center, double angle) const {
-    angle = (angle / 180) * PI;
+    angle = (angle / 180) * NUM_PI;
     double x = center.x + cos(angle) * (this->x - center.x) - sin(angle) * (this->y - center.y);
     double y = center.y + sin(angle) * (this->x - center.x) + cos(angle) * (this->y - center.y);
     return Point(x, y);
   }
 
-  void reflex(const Point& center) {
+  void reflect(const Point& center) {
     x = 2 * center.x - x;
     y = 2 * center.y - y;
   }
 
-  void reflex(const Line& axis);
+  void reflect(const Line& axis);
 
   void scale(const Point& center, double coefficient) {
     x = (x - center.x) * coefficient + center.x;
@@ -70,7 +70,6 @@ public:
 
   Line(double k, double b) : a(-k), b(1), c(-b) {}
 
-  friend bool operator==(const Line&, const Line&);
 };
 
 bool operator==(const Line& line1, const Line& line2) {
@@ -100,25 +99,29 @@ Line middle_perpendicular(const Point& point1, const Point& point2) {
   return result;
 }
 
-void Point::reflex(const Line& axis) {
+void Point::reflect(const Line& axis) {
   Line perpendicular1 = perpendicular(axis);
   perpendicular1.c = -perpendicular1.a * x - perpendicular1.b * y;
   scale(cross(perpendicular1, axis), -1);
 }
 
 class Vector {
+public:
   double x = 0.0;
   double y = 0.0;
-public:
+
   Vector(double x, double y) : x(x), y(y) {}
 
   Vector(const Point& a, const Point& b) : x(a.x - b.x), y(a.y - b.y) {}
 
-  friend double scalarProduct(const Vector&, const Vector&);
 };
 
-double scalarProduct(const Vector& vec1, const Vector& vec2) {
+double ScalarProduct(const Vector& vec1, const Vector& vec2) {
   return vec1.x * vec2.x + vec1.y * vec2.y;
+}
+
+double VectorMultiply(const Vector& vec1, const Vector& vec2) {
+  return vec1.x * vec2.y - vec2.x * vec1.y;
 }
 
 class Shape {
@@ -139,36 +142,58 @@ public:
 
   virtual void rotate(Point center, double angle) = 0;
 
-  virtual void reflex(Point center) = 0;
+  virtual void reflect(Point center) = 0;
 
-  virtual void reflex(Line axis) = 0;
+  virtual void reflect(Line axis) = 0;
 
   virtual void scale(Point center, double k) = 0;
 
   virtual ~Shape() = 0;
 };
 
-class Polygon : Shape {
+Shape::~Shape() {}
+
+class Segment {
+public:
+  Point p1;
+  Point p2;
+  Segment() = default;
+
+  Segment(double x1, double y1, double x2, double y2) {
+    p1 = Point(x1, y1);
+    p2 = Point(x2, y2);
+  }
+  Segment(const Point &p1, const Point &p2) : p1(p1), p2(p2) {}
+  bool ContainsPoint(const Point& point) const {
+    Vector point_to_first(p1, point);
+    Vector point_to_second(p2, point);
+    return (VectorMultiply(point_to_first, point_to_second) == 0
+            && ScalarProduct(point_to_first, point_to_second) <= 0);
+  }
+};
+
+class Polygon : public Shape {
 protected:
   std::vector<Point> vertices_;
-  int vertices_count_;
 public:
   Polygon() = default;
 
-  Polygon(std::vector<Point>& vertices) : vertices_(vertices), vertices_count_(static_cast<int>(vertices.size())) {}
+  Polygon(std::vector<Point>& vertices) : vertices_(vertices) {}
 
-  Polygon(std::initializer_list<Point> vertices) : vertices_(vertices),
-                                                   vertices_count_(static_cast<int>(vertices.size())) {}
+  template<class... Types>
+  Polygon(Types... args) {
 
-  int verticesCount() const { return vertices_count_; };
+  }
+
+  int verticesCount() const { return static_cast<int>(vertices_.size()); };
 
   std::vector<Point> getVertices() { return vertices_; };
 
   bool isConvex() {
-    for (int i = 0; i < vertices_count_; ++i) {
-      Vector vec1 = Vector(vertices_[i], vertices_[(i + 1) % vertices_count_]);
-      Vector vec2 = Vector(vertices_[(i + 1) % vertices_count_], vertices_[(i + 2) % vertices_count_]);
-      if (scalarProduct(vec1, vec2) < 0) {
+    for (int i = 0; i < verticesCount(); ++i) {
+      Vector vec1 = Vector(vertices_[i], vertices_[(i + 1) % verticesCount()]);
+      Vector vec2 = Vector(vertices_[(i + 1) % verticesCount()], vertices_[(i + 2) % verticesCount()]);
+      if (ScalarProduct(vec1, vec2) < 0) {
         return false;
       }
     }
@@ -197,15 +222,35 @@ public:
     auto polygon_ptr = dynamic_cast<const Polygon*>(&another);
     if (!polygon_ptr) return false;
     if (verticesCount() != polygon_ptr->verticesCount()) return false;
-    //!!!
+    Point first = polygon_ptr->vertices_[0];
+    for (int i = 0; i < verticesCount(); ++i) {
+      if (vertices_[i] == first) {
+        bool is_equal = true;
+        for (int j = 0; j < verticesCount(); ++j) {
+          if (polygon_ptr->vertices_[j] != vertices_[(i + j) % verticesCount()]) {
+            is_equal = false;
+            break;
+          }
+        }
+        if (is_equal) return true;
+        is_equal = true;
+        for (int j = 0; j < verticesCount(); ++j) {
+          int k;
+          if (i - j < 0) k = verticesCount() + (i - j);
+          else k = i - j;
+          if (polygon_ptr->vertices_[j] != vertices_[k]) {
+            is_equal = false;
+            break;
+          }
+        }
+        if (is_equal) return true;
+      }
+    }
+    return false;
   }
 
   bool operator!=(const Shape& another) const override {
     return !(*this == another);
-  }
-
-  bool isCongruentTo(const Shape& another) const override {
-    //!!!
   }
 
   bool isSimilarTo(const Shape& another) const override {
@@ -215,24 +260,49 @@ public:
     return area() / ptr->area() == k * k;
   }
 
+  bool isCongruentTo(const Shape& another) const override {
+    return this->isSimilarTo(another) && this->area() == another.area();
+  }
+
   bool containsPoint(Point point) const override {
-    //!!!
+    for (int i = 0; i < verticesCount(); ++i) {
+      auto seg = Segment(vertices_[i], vertices_[(i + 1) % verticesCount()]);
+      if (seg.ContainsPoint(point)) {
+        return true;
+      }
+    }
+    bool is_inside = false;
+    int j = verticesCount() - 1;
+    for (int i = 0; i < verticesCount(); ++i) {
+      // false if there is an even number of sides of poly on the left and true with an odd one.
+      bool one_direction = vertices_[i].y < point.y && vertices_[j].y >= point.y;
+      bool other_direction = vertices_[j].y < point.y && vertices_[i].y >= point.y;
+      if (one_direction || other_direction) {
+        double ray_poly_cross_x = vertices_[i].x + (point.y - vertices_[i].y) /
+                                                   (vertices_[j].y - vertices_[i].y) * (vertices_[j].x - vertices_[i].x);
+        if (ray_poly_cross_x < point.x) {
+          is_inside = !is_inside;
+        }
+      }
+      j = i;
+    }
+    return is_inside;
   }
 
   void rotate(Point center, double angle) override {
-    angle = (angle / 180) * PI;
+    angle = (angle / 180) * NUM_PI;
     for (int i = 0; i < verticesCount(); ++i) {
       vertices_[i] = vertices_[i].rotate(center, angle);
     }
   }
 
-  void reflex(Point center) override {
+  void reflect(Point center) override {
     scale(center, -1);
   }
 
-  void reflex(Line axis) override {
+  void reflect(Line axis) override {
     for (int i = 0; i < verticesCount(); ++i) {
-      vertices_[i].reflex(axis);
+      vertices_[i].reflect(axis);
     }
   }
 
@@ -244,7 +314,7 @@ public:
   }
 };
 
-class Ellipse : Shape {
+class Ellipse : public Shape {
   // x^2/a^2 + y^2/b^2 = 1;
 protected:
   double a = 0.0;
@@ -274,11 +344,11 @@ public:
   }
 
   double perimeter() const override {
-    return PI * (3 * (a + b) - sqrt((3 * a + b) * (a + 3 * b)));
+    return NUM_PI * (3 * (a + b) - sqrt((3 * a + b) * (a + 3 * b)));
   }
 
   double area() const override {
-    return PI * a * b;
+    return NUM_PI * a * b;
   }
 
   bool operator==(const Shape& another) const override {
@@ -313,18 +383,18 @@ public:
   }
 
   void rotate(Point center, double angle) override {
-    angle *= PI / 180;
+    angle *= NUM_PI / 180;
     focus.first = focus.first.rotate(center, angle);
     focus.second = focus.second.rotate(center, angle);
   }
 
-  void reflex(Point center) override {
+  void reflect(Point center) override {
     scale(center, -1);
   }
 
-  void reflex(Line axis) override {
-    focus.first.reflex(axis);
-    focus.second.reflex(axis);
+  void reflect(Line axis) override {
+    focus.first.reflect(axis);
+    focus.second.reflect(axis);
   }
 
   void scale(Point center, double k) override {
@@ -349,7 +419,7 @@ public:
   Rectangle() : Polygon() {};
 
   Rectangle(const Point& point1, const Point& point2, double k) : Polygon{point1, Point(0, 0), point2, Point(0, 0)} {
-    double angle = 180 - 2 * atan(k) * 180 / PI;
+    double angle = 180 - 2 * atan(k) * 180 / NUM_PI;
     Point middle = Point((point1.x + point2.x) / 2, (point1.y + point2.y) / 2);
     vertices_[1] = point1.rotate(middle, angle);
     vertices_[3] = point2.rotate(middle, angle);
